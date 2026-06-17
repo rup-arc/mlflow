d49cec0c02378b41bf0d67923d0c92a48b10045f
Ubuntu <ubuntu@ip-172-31-22-134.ap-south-1.compute.internal>
2026-06-17 08:04:09 +0000

    Add MLflow model inference API

--- app/app.py
+++ app/app.py
@@ -1,16 +1,35 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import mlflow.pyfunc
+import pandas as pd
 
 app = Flask(__name__)
 
+MODEL_URI = "mlruns/0/models/m-1a7b1e21508b4aa791e6aa1de6ba80f0/artifacts"
+
+model = mlflow.pyfunc.load_model(MODEL_URI)
+
 @app.route("/")
 def home():
-    return {
+    return jsonify({
         "status": "running",
-        "service": "mlops-devops-project"
-    }
+        "model_loaded": True
+    })
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    data = request.get_json()
+
+    features = pd.DataFrame([data])
+
+    prediction = model.predict(features)
+
+    return jsonify({
+        "prediction": int(prediction[0])
+    })
 
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
-        port=5000
+        port=8080
     )
